bf2c98e398f871828f77df243e1426bc54ca7857
unknown <a20151107@pucp.pe>
2019-04-24 01:41:40 -0500

    First commit

---
+++ FrontEnd/app/app.js
@@ -0,0 +1,24 @@
+var vHackersModule = angular.module('vHackersModule', []);
+
+//Se ejecuta antes de que corra la aplicacion
+vHackersModule.config(function () {
+
+});
+//Se ejecuta mientras corre la aplicacion
+// vHackersModule.run(function () {
+//
+// });
+vHackersModule.controller('HackersCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.mensaje = "Hola Mundo";
+  $scope.amigos = [];
+  $scope.probar = function () {
+    $http({
+        method: 'GET',
+        url: 'data/Hackers.json'
+     }).then(function (response) {
+      $scope.amigos = response.data;
+    }, function (error) {
+
+    });
+  };
+}]);
