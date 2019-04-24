416dc0570027235e380ce9b97f6354ba599fe8a1
unknown <a20151107@pucp.pe>
2019-04-24 03:43:49 -0500

    commit mas

--- FrontEnd/app/app.js
+++ FrontEnd/app/app.js
@@ -24,34 +24,3 @@ vHackersModule.controller('HackersCtrl', ['$scope', '$http', function($scope, $h
     });
   };
 }]);
-=======
-var vHackersModule = angular.module('vHackersModule', []);
-
-//Se ejecuta antes de que corra la aplicacion
-vHackersModule.config(function () {
-
-});
-//Se ejecuta mientras corre la aplicacion
-// vHackersModule.run(function () {
-//
-// });
-vHackersModule.controller('HackersCtrl', ['$scope', '$http', function($scope, $http){
-<<<<<<< HEAD
-  var ctrl = this;
-  ctrl.mensaje = "Hola Mundo";
-  ctrl.amigos = [];
-  ctrl.mensajeNuevo = "Go V-Hackers";
-  ctrl.probar = function () {
-  $scope.probar = function () {
->>>>>>> 6563f1fd9efbecc1d9d234f9f3287494b32278d3
-    $http({
-        method: 'GET',
-        url: 'data/Hackers.json'
-     }).then(function (response) {
-      ctrl.amigos = response.data;
-    }, function (error) {
-
-    });
-  };
-}]);
->>>>>>> before discard
