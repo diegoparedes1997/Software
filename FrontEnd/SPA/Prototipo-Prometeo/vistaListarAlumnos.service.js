5361925bb546991b313fe98ab40c047892ef7726
Carlos <carlosg.carrillo@pucp.pe>
2019-04-28 11:08:33 -0500

    Se actulizó el URL

--- FrontEnd/SPA/Prototipo-Prometeo/vistaListarAlumnos.service.js
+++ FrontEnd/SPA/Prototipo-Prometeo/vistaListarAlumnos.service.js
@@ -6,7 +6,7 @@ function($q, $http) {
 
   servicio.listarAlumnos = function(){
 
-    var urlListarAlumnos = 'data/Hackers.json';
+    var urlListarAlumnos = 'http://200.16.7.187:7000/users';
     var $defer = $q.defer();
     $http({
         method: 'GET',
