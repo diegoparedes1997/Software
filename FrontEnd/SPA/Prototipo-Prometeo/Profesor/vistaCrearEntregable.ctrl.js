3d2f834af4e419269ee7150309f90d7a6f8b00c6
Anderson93PUCP <anderson.castillo@pucp.pe>
2019-05-02 21:18:20 -0500

    Modificaciones de Vistas 20190502 2118

--- FrontEnd/SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.ctrl.js
+++ FrontEnd/SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.ctrl.js
@@ -1,14 +1,13 @@
 //-- Anderson
-angular.module('vHackersModule').controller('entregableCtrl', ['$scope', 'gestiónProyectoService', '$uibModal',
-function($scope, gestiónProyectoService, $uibModal){
+angular.module('vHackersModule').controller('entregableCtrl', ['$scope', 'entregableService', '$uibModal',
+function($scope, entregableService, $uibModal){
   var ctrl = this;
-  var entregable=this;
-  ctrl.titulo = "Nuevo Entregable";
-  ctrl.alumnosLista = [];
-  ctrl.alumnosListaModal = [];
-  $scope.columnas=[1,2,3,4,5,6,7];
-  $scope.filas=[1,2,3,4];
+  ctrl.tituloNuevo = "Nuevo Entregable";
+  ctrl.tituloModificado= "Entregable";
+  ctrl.filas=[1,2,3,4];
   ctrl.dias=['D','L','M','Mi','J','V','S'];
+  ctrl.columnas=[1,2,3,4,5,6,7];
+  ctrl.entregableM=[];
   // var d=new Date("5/11/2020");
   // $scope.entregable={nombEntrg: 'Entregable 1', fechaI:new Date("2/05/2019"), horaI:new Date("2/05/2019 20:08"),
   //                     descrEntrg: 'Entregable 1', fechaF:new Date("2/05/2019"),horaF:new Date("2/05/2019 20:08"),
@@ -18,43 +17,56 @@ function($scope, gestiónProyectoService, $uibModal){
 
 
 
-  ctrl.probar = function () {
-    gestiónProyectoService.listarAlumnos().then(function (alumnosListaData) {
-      ctrl.alumnosLista = alumnosListaData;
+  $scope.init = function () {
+    entregableService.entregableAlumno().then(function (entregableData) {
+      ctrl.entregableM = entregableData[0];
+      ctrl.entregableM[0].fechaEntrega=new Date(Date.now());
+      ctrl.entregableM[0].descripcion="Entregable recibido con éxito";
     });
   };
 
-  ctrl.probarModal = function () {
-    //En este caso el controlador del modal se debe declarar en el JSON que pasa como parametro de open
-    var modalInstance = $uibModal.open({
-      animation: false,
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html',
-      controller: 'entregableCtrl as ctrl',
-      size: 'lg',
-      backdrop: true,
-      keyboard: true,
-      resolve: {
-        parametrosModalListarAlumnos: function (){
-          return "V Hackers"
-        }
-      }
-    });
+  // ctrl.entregableM =function() { entregableService.entregableAlumno() };
 
-    //Recibo parametro de retorno
-    modalInstance.result.then( function (parametroRetorno) {
-      ctrl.alumnosLista.push(parametroRetorno);
-    });
-  };
+
+
   ctrl.swalEntregable = function (entregable) {
+    entregable.id=0;
+    entregable.tieneAlarma=1;
     console.log(angular.toJson(entregable));//Envio el json para crear el entregable
-    entregable.nombEntrg="";
-    entregable.fechaI="";
-    entregable.horaI="";
-    entregable.fechaF="";
-    entregable.horaF="";
-    entregable.descrEntrg="";
-    entregable.puntaje="";
+    entregable.nombre="";
+    entregable.fechaEntrega="";
+    entregable.horaEntrega="";
+    entregable.fechaFin="";
+    entregable.horaFin="";
+    entregable.descripcion="";
+    entregable.ponderacion="";
     swal("¡Bien hecho!", "El entregable se creo exitosamente" , "success");
+  };
+
+  ctrl.swalEntregableM = function (entregableM) {
+    if (entregableM.id==0) {
+      entregableM.id=0;
+    }
+    entregableM.tieneAlarma=1;
+    console.log(angular.toJson(entregableM));//Envio el json para crear el entregable
+    data=[{
+      "id": entregableM.id,
+      "nombre": entregableM.nombre,
+      "fechaEntrega": entregableM.fechaEntrega,
+      "tieneAlarma": 1,
+      "ponderacion": entregableM.ponderacion
+      }]
+    entregableService.registroentregableAlumno(data).then(function () {
+      ctrl.exitoso="Entregable enviado con éxito";
+    });
+    entregableM.nombre="";
+    entregableM.fechaEntrega="";
+    entregableM.horaEntrega="";
+    entregableM.fechaFin="";
+    entregableM.horaFin="";
+    entregableM.descripcion="";
+    entregableM.ponderacion="";
+    swal("¡Bien hecho!", "El entregable se modificó exitosamente" , "success");
   };
 
 
--- FrontEnd/SPA/Prototipo-Prometeo/Profesor/vistaGestiónEntregable.service.js
+++ FrontEnd/SPA/Prototipo-Prometeo/Profesor/vistaGestiónEntregable.service.js
@@ -1,16 +1,34 @@
 //El $q aún se está evaluando si se va a emplear
-angular.module("vHackersModule").service('gestiónEntregableService', ['$q', '$http',
+angular.module("vHackersModule").service('entregableService', ['$q', '$http',
 function($q, $http) {
 
   var servicio = this;
 
-  servicio.listarAlumnos = function(){
+  servicio.entregableAlumno = function(){
 
-    var urlListarAlumnos = '';
+    var urlEntregableAlumno = 'data/Entregable.json';
     var $defer = $q.defer();
     $http({
         method: 'GET',
-        url: urlListarAlumnos
+        url: urlEntregableAlumno
+     }).then(function (respuesta) {
+       $defer.resolve(respuesta.data);
+     }).catch(function (error) {
+       $defer.reject(error);
+     });
+    return $defer.promise;
+  }
+
+
+  servicio.registroentregableAlumno = function(data){
+
+    var urlRegistroEntregableAlumno = 'http://127.0.0.1:5000/';
+    var data=JSON.stringify(data);
+    var $defer = $q.defer();
+    $http({
+        method: 'POST',
+        url: urlRegistroEntregableAlumno,
+        data: data
      }).then(function (respuesta) {
        $defer.resolve(respuesta.data);
      }).catch(function (error) {
