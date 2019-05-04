68e39565f49c6e5697e76c96958c418b14c4d152
Alejo14 <a.tapiat@pucp.pe>
2019-05-04 11:34:55 -0500

    Se arreglo el stateHelperProvider y las vistas

--- FrontEnd/SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.ctrl.js
+++ FrontEnd/SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.ctrl.js
@@ -6,16 +6,25 @@ function calificacionCtrl ($scope, NgTableParams,herramientaEvaluacionServicio){
   var ctrl = this;
   ctrl.titulo = "Exposición de Arquitectura de microservicios";
   ctrl.retroalimentacion = "";
-  ctrl.puntajeMax = 20;
   ctrl.puntaje = 0;
   ctrl.id = 0;
+
+  /*===============================================
+  * Servicio para traer Herramienta de Evaluación
+  *================================================
+  */
   ctrl.herramientaEvaluacion = [];
   ctrl.obtenerHerramientaEvaluacion = function () {
     herramientaEvaluacionServicio.obtenerHerramientaEvaluacion().then(function (herramientaEvaluacion) {
-      ctrl.herramientaEvaluacion = herramientaEvaluacion;
+      ctrl.herramientaEvaluacion = herramientaEvaluacion.herramientaEvaluacion;
+      ctrl.puntajeMax = herramientaEvaluacion.puntajeMax;
     });
   };
 
+  /*===============================================
+  * JSON de evaluación
+  *================================================
+  */
   ctrl.evaluacion =
   {
   "id": ctrl.id,
@@ -23,12 +32,25 @@ function calificacionCtrl ($scope, NgTableParams,herramientaEvaluacionServicio){
   "retroalimentacion" : ctrl.retroalimentacion
   };
 
+  /*===============================================
+  * Servicio para enviar Calificación al Back-End
+  *================================================
+  */
   ctrl.enviarCalificacion = function() {
     herramientaEvaluacionServicio.enviarCalificacion(ctrl.enviarCalificacion);
   };
 
-  ctrl.sumarPuntaje = function(value){
-    ctrl.puntaje += parseInt(value);
+  /*===============================================
+  * Puntaje dinámico: Función temporal
+  *================================================
+  */
+  ctrl.puntajeDado = [-1,-1,-1,-1,-1];
+  ctrl.sumarPuntaje = function(value, id){
+    var aux = -1;
+    if(ctrl.puntajeDado[id] != -1) aux = ctrl.puntajeDado[id];
+    ctrl.puntajeDado[id] = parseInt(value);
+    if (aux != -1) ctrl.puntaje -= aux;
+    ctrl.puntaje += ctrl.puntajeDado[id];
   };
 
   ctrl.init = function (){
--- FrontEnd/app/app.js
+++ FrontEnd/app/app.js
@@ -1,50 +1,57 @@
-var vHackersModule = angular.module('vHackersModule', ['ngRoute', 'ui.bootstrap', 'ngTable']);
+var vHackersModule = angular.module('vHackersModule', ['ui.bootstrap', 'ngTable','ui.router','ui.router.stateHelper']);
 
 //Se ejecuta antes de que corra la aplicacion
-vHackersModule.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    .when('/inicio', {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaPrincipal.html'
-    })
-    .when('/listaAlumnos', {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
-    })
-    .when('/tabla' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaTabla.html'
-    })
-    .when('/calificacion', {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.html'
-<<<<<<< HEAD
-    })
-      .when('/Profesor' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
-      })
-      .when('/Profesor/Curso' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
-      })
-      .when('/Profesor/GestionProyecto' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
-      })
-      .when('/entregable' , {
-          templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
-=======
-    })
-    .when('/Profesor' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
-    })
-    .when('/Profesor/Curso' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
-    })
-    .when('/Profesor/GestionProyecto' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
-    })
-    .when('/entregable' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
->>>>>>> 5f1a306881878c04140d6952b10f77e435321dce
-    })
-    .otherwise({
-      redirectTo: '/inicio'
-    })
+vHackersModule.config(['$urlRouterProvider', 'stateHelperProvider',
+function ($urlRouterProvider,stateHelperProvider) {
+  stateHelperProvider
+  .state({
+      name: 'index',
+      abstract: true,
+      url: '/index',
+      templateUrl: 'index.html',
+      children:[
+        {
+          name: 'inicio',
+          url: '/inicio',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaPrincipal.html'
+        },
+        {
+          name: 'listaAlumnos',
+          url: '/listaAlumnos',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
+        },
+        {
+          name: 'tabla',
+          url: '/tabla',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaTabla.html'
+        },
+        {
+          name: 'calificacion',
+          url: '/calificacion',
+          templateUrl: 'SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.html'
+        },
+        {
+          name: 'profesor',
+          url: '/profesor',
+          templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
+        },
+        {
+          name: 'curso',
+          url: '/curso',
+          templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
+        },
+        {
+          name:'gestion-proyecto',
+          url: '/gestion-proyecto',
+          templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
+        },
+        {
+            name: 'evaluacion-herramienta',
+            url: '/evaluacion-herramienta',
+            templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
+        }
+      ]
+    });
 }]);
 //Se ejecuta mientras corre la aplicacion
 // vHackersModule.run(function () {
