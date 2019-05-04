dab1d26a0b9d4eced33e03cacaa12e5d91f47970
Alejo14 <a.tapiat@pucp.pe>
2019-05-04 11:41:30 -0500

    Se arreglaron dependencias, la navegación y los estilos de la aplicación

--- FrontEnd/app/app.js
+++ FrontEnd/app/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var vHackersModule = angular.module('vHackersModule', ['ui.bootstrap', 'ngTable','ui.router','ui.router.stateHelper']);
 
 //Se ejecuta antes de que corra la aplicacion
@@ -53,55 +52,6 @@ function ($urlRouterProvider,stateHelperProvider) {
         }
       ]
     });
-=======
-var vHackersModule = angular.module('vHackersModule', ['ngRoute', 'ui.bootstrap', 'ngTable', 'angular.chosen']);
-
-//Se ejecuta antes de que corra la aplicacion
-vHackersModule.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    .when('/inicio', {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaPrincipal.html'
-    })
-    .when('/listaAlumnos', {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
-    })
-    .when('/tabla' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaTabla.html'
-    })
-    .when('/gestion-usuarios' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Administrador/Gestion-usuarios/gestionUsuarios.html'
-    })
-    .when('/calificacion', {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.html'
-    })
-      .when('/Profesor' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
-      })
-      .when('/Profesor/Curso' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
-      })
-      .when('/Profesor/GestionProyecto' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
-      })
-      .when('/Profesor' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
-      })
-      .when('/Profesor/Curso' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
-      })
-      .when('/Profesor/GestionProyecto' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
-      })
-      .when('/entregable' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
-      })
-      .when('/entregableM' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaModificarEntregable.html'
-      })
-      .otherwise({
-        redirectTo: '/inicio'
-      })
->>>>>>> 5cc0e24aaa2b9f4f2168011b0603cf3029525668
 }]);
 //Se ejecuta mientras corre la aplicacion
 // vHackersModule.run(function () {
