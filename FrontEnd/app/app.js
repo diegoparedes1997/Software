1a83de308fa38a658a888db0068bdcbb5e2f5c5c
unknown <a20151107@pucp.pe>
2019-05-04 11:42:34 -0500

    Cambios en ruteo

--- FrontEnd/app/app.js
+++ FrontEnd/app/app.js
@@ -1,51 +1,91 @@
-var vHackersModule = angular.module('vHackersModule', ['ngRoute', 'ui.bootstrap', 'ngTable', 'angular.chosen']);
+var vHackersModule = angular.module('vHackersModule', ['ui.router', 'ui.router.stateHelper', 'ui.bootstrap', 'ngTable', 'angular.chosen']);
 
 //Se ejecuta antes de que corra la aplicacion
-vHackersModule.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    .when('/inicio', {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaPrincipal.html'
-    })
-    .when('/listaAlumnos', {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
-    })
-    .when('/tabla' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/vistaTabla.html'
-    })
-    .when('/gestion-usuarios' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Administrador/Gestion-usuarios/gestionUsuarios.html'
-    })
-    .when('/calificacion', {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.html'
-    })
-      .when('/Profesor' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
-      })
-      .when('/Profesor/Curso' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
-      })
-      .when('/Profesor/GestionProyecto' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
-      })
-      .when('/entregable' , {
-          templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
-    })
-    .when('/Profesor' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
-    })
-    .when('/Profesor/Curso' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
-    })
-    .when('/Profesor/GestionProyecto' , {
-      templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
-    })
-    .when('/entregable' , {
-        templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
-    })
-    .otherwise({
-      redirectTo: '/inicio'
-    })
+vHackersModule.config([
+  '$urlRouterProvider',
+  'stateHelperProvider',
+  function($urlRouterProvider, stateHelperProvider) {
+    $urlRouterProvider.otherwise('raiz.inicio');
+    stateHelperProvider
+    .state({
+      name: 'raiz',
+      abstract: true,
+      url: '/raiz',
+      templateUrl: 'SPA/raiz.html',
+      children: [
+        {
+          name: 'inicio',
+          url : '/inicio',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaPrincipal.html'
+        },
+        {
+          name: 'listaAlumnos',
+          url : '/listaAlumnos',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
+        },
+        {
+          name: 'tabla',
+          url : '/tabla',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaTabla.html'
+        },
+        {
+          name: 'listaAlumnos',
+          url : '/listaAlumnos',
+          templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
+        },
+        {
+          name: 'gestion-usuarios',
+          url : '/gestion-usuarios',
+          templateUrl: 'SPA/Prototipo-Prometeo/Administrador/Gestion-usuarios/gestionUsuarios.html'
+        }]
+      });
 }]);
+
+// vHackersModule.config(['$routeProvider', function ($routeProvider) {
+//   $routeProvider
+//     .when('/inicio', {
+//       templateUrl: 'SPA/Prototipo-Prometeo/vistaPrincipal.html'
+//     })
+//     .when('/listaAlumnos', {
+//       templateUrl: 'SPA/Prototipo-Prometeo/vistaListarAlumnos.html'
+//     })
+//     .when('/tabla' , {
+//       templateUrl: 'SPA/Prototipo-Prometeo/vistaTabla.html'
+//     })
+//     .when('/gestion-usuarios' , {
+//       templateUrl: 'SPA/Prototipo-Prometeo/Administrador/Gestion-usuarios/gestionUsuarios.html'
+//     })
+//     .when('/calificacion', {
+//       templateUrl: 'SPA/Prototipo-Prometeo/Profesor/Evaluacion-Retroalimentacion/calificacionEntregable.html'
+//     })
+//       .when('/Profesor' , {
+//         templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
+//       })
+//       .when('/Profesor/Curso' , {
+//         templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
+//       })
+//       .when('/Profesor/GestionProyecto' , {
+//         templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
+//       })
+//       .when('/entregable' , {
+//           templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
+//     })
+//     .when('/Profesor' , {
+//       templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaInicio.html'
+//     })
+//     .when('/Profesor/Curso' , {
+//       templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCurso.html'
+//     })
+//     .when('/Profesor/GestionProyecto' , {
+//       templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaGestiónProyecto.html'
+//     })
+//     .when('/entregable' , {
+//         templateUrl: 'SPA/Prototipo-Prometeo/Profesor/vistaCrearEntregable.html'
+//     })
+//     .otherwise({
+//       redirectTo: '/inicio'
+//     })
+// }]);
 //Se ejecuta mientras corre la aplicacion
 // vHackersModule.run(function () {
 //
