ef11b3fbe39a04feebb4e5d9fc1cb2fd328944df
unknown <a20151107@pucp.pe>
2019-05-04 20:07:17 -0500

    Se cambio el puerto del servidor

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -7,5 +7,5 @@ app.get('/inicio/principal', function (req, res) {
     res.redirect('/inicio/principal');
 });
 
-app.listen(8080, 'localhost');
+app.listen(8070, 'localhost');
 console.log("MyProject Server is Listening on port 8080");
